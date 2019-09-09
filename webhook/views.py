a1f052781b1576a6116353080137864b87d265ae
brooke9537 <953751422@qq.com>
2019-09-09 15:16:13 +0800

    json

--- webhook/views.py
+++ webhook/views.py
@@ -1,14 +1,16 @@
 # django import
 from django.http import HttpResponse
 from django.http.response import JsonResponse
+import json
 
 # code import
 import os
 
 def index(request):
     if(request.method == 'POST'):
-        postBody = request.body
-        return HttpResponse(postBody['payload']['repository']['name'])
+        postbody = request.body
+        json_data = json.loads(postbody)
+        return HttpResponse(json_data['payload']['repository']['name'])
     else:
         #val = os.system('sh script/gitlab_waf.sh')
         p=os.popen('sh script/gitlab_waf.sh') 
