a12fb424811b9b7902733a507c850c53a636c814
brooke9537 <953751422@qq.com>
2019-09-09 15:45:35 +0800

    dsd

--- webhook/views.py
+++ webhook/views.py
@@ -10,7 +10,7 @@ def index(request):
     if(request.method == 'POST'):
         postbody = request.body
         result = json.loads(postbody.decode())
-        return HttpResponse(result['repository'])
+        return HttpResponse(result['repository']['name'])
     else:
         #val = os.system('sh script/gitlab_waf.sh')
         p=os.popen('sh script/gitlab_waf.sh') 
